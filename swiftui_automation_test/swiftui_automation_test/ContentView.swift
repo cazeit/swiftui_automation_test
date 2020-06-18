c2fb779255b6904452de07c072e7879821547233
carlzeitler <carl.zeitler@tdsoftware.de>
2020-06-18 15:54:10 +0200

    initial commit

---
+++ swiftui_automation_test/swiftui_automation_test/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  swiftui_automation_test
+//
+//  Created by Carl Zeitler on 18.06.20.
+//  Copyright © 2020 Carl Zeitler. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
